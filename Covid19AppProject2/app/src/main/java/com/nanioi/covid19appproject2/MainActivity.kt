a5c6c33972c69990d5040007d1bb90877b019b58
Nanioi <0802ny@gmail.com>
2022-02-11 18:00:47 +0900

    bottomNavigation, Fragment 추가
    - 기존 Activity끼리 이동 => Fragment 사용
    - bottomNavigation 추가

--- Covid19AppProject2/app/src/main/java/com/nanioi/covid19appproject2/MainActivity.kt
+++ Covid19AppProject2/app/src/main/java/com/nanioi/covid19appproject2/MainActivity.kt
@@ -2,10 +2,39 @@ package com.nanioi.covid19appproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val summaryFragment = SummaryFragment()
+//        val NewFragment = NewFragment()
+//        val clinicFragment = ClinicFragment()
+//        val precautionFragment = PrecautionFragment()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        replaceFragment(summaryFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.summary -> replaceFragment(summaryFragment)
+//                R.id.New -> replaceFragment(NewFragment)
+//                R.id.clinic -> replaceFragment(clinicFragment)
+//                R.id.precaution -> replaceFragment(precautionFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainer, fragment)
+                commit()
+            }
     }
 }
